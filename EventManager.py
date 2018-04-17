e08dfc168a989d097d61663ecff0a4a6f20b9e97
z5159267 <z5159267@cse.unsw.edu.au>
2018-04-17 16:18:39 +1000

    added some functions, some are yet to be implemented as UML diagram is redundant

--- EventManager.py
+++ EventManager.py
@@ -1,2 +1,34 @@
 from Event import *
 from User import *
+
+class EventManager():
+    def __init__(self):
+        self._venues : []
+        self._events: []
+    
+    def getVenues(self):
+        return self._venues
+        
+    def getEvents(self):
+        return self._events
+  
+    def addVenue(self, venue):
+        self._venues.append(venue)
+    
+    def addEvent(self, event):
+        self._events.append(event)
+        
+    def addAttendee(self, event, attendee):
+        for x in self._events:
+            if (x == event):
+                return x.addAttendee(attendee)
+            
+    # what is this function supposed to do???
+    def changeEvent(self, event):
+        pass
+        
+        
+    def getVenue(self, event):
+        for x in self._events:
+            if (x == event):
+                return x.getVenue
