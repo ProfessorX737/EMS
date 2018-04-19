a1b201f1da9741eabcbb348354a61d8af86cc447
Xavier Poon <>
2018-04-19 22:02:16 +1000

    fixed the inits

--- Course.py
+++ Course.py
@@ -1,8 +1,8 @@
 from Event import Event
 
 class Course(Event):
-    def __init__(self,period,venue,convener,capacity,deregEnd):
-        super().__init__(self,period,venue,convener,capacity,deregEnd)
+    def __init__(self,startDateTime,endDateTime,name,descr,venue,convener,capacity,deregEnd):
+        super().__init__(self,startDateTime,endDateTime,name,descr,venue,convener,capacity,deregEnd)
     
     def getPresenter(self):
         return self.getConvener()
