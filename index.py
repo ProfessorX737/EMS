bf312c987a08507fe3e3d0c1af87f0d61b4ea9d9
Ho Kim <hkim98@live.com.au>
2018-05-01 23:47:19 +1000

    Fixed

--- index.py
+++ index.py
@@ -18,8 +18,6 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
-# userManager = UserManager()
-# eventManager = EventManager()
 ems = EventManagementSystem()
 
 @login_manager.user_loader
