a813c2e32342a3759b06315f282e900561c5ed88
Ho Kim <hkim98@live.com.au>
2018-05-06 18:55:26 +1000

    validator messages

--- CreateEventForm.py
+++ CreateEventForm.py
@@ -1,6 +1,7 @@
 from flask_wtf import Form
 from wtforms import TextField, TextAreaField, validators, StringField, DateTimeField, IntegerField, SubmitField, SelectField
-
+from validators.dateTime_isLessThan import *
+from validators.dateTime_isGreaterThan import *
 # Enables us to pass parameters into WTForm 
 class NewStartUpForm( Form ):
     def __init__( self, venues, *arg, **kwarg ):
@@ -14,9 +15,9 @@ class CreateEventForm(Form):
     eventType = SelectField('Event Type', choices=eventChoices,validators=(validators.Optional(),))
     name = StringField('Name', validators=[validators.DataRequired("Please enter event name.")])
     description = StringField('Description', validators=[validators.DataRequired("Please enter event description.")])
-    startDateTime = DateTimeField('Start Date Time', format='%Y-%m-%d %H:%M', validators=[validators.DataRequired("Please enter the event start date and time.")])
-    endDateTime = DateTimeField('End Date Time', format='%Y-%m-%d %H:%M', validators=[validators.DataRequired("Please enter the event end date and time.")])
-    deregEnd = DateTimeField('Deregister Period End', format='%Y-%m-%d %H:%M', validators=[validators.DataRequired("Please enter the end of deregistration period.")])
+    startDateTime = DateTimeField('Start Date Time', format='%Y-%m-%d %H:%M', validators=[validators.DataRequired("Please enter the event start date and time."),LessThan('endDateTime','start < end')])
+    endDateTime = DateTimeField('End Date Time', format='%Y-%m-%d %H:%M', validators=[validators.DataRequired("Please enter the event end date and time."), GreaterThan('startDateTime','end > start')])
+    deregEnd = DateTimeField('Deregister Period End', format='%Y-%m-%d %H:%M', validators=[validators.DataRequired("Please enter the end of deregistration period."), LessThan('startDateTime', 'dereg < start')])
     convener = StringField('Convener Name', validators=[validators.DataRequired("Please enter event convener's name.")])
     venue = SelectField('Venue')
     capacity = IntegerField('Capacity', validators=[validators.DataRequired("Please enter the event capacity.")])
